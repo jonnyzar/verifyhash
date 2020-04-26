ec74386cc91f314afae60bb74c162088b542743c
jonnyzar <64309336+jonnyzar@users.noreply.github.com>
2020-04-26 16:46:48 +0200

    V

--- verifyhash.py
+++ verifyhash.py
@@ -62,3 +62,34 @@ def hashfile(srcFilePath, hashType, BLOCK_SIZE=65536):
     except Exception as e:
         print(e)
         exit(1)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('srcHash', type=str)
+    parser.add_argument('srcFilePath', type=str)
+
+    args = parser.parse_args()
+
+    srcHash = args.srcHash
+    srcFilePath = args.srcFilePath
+
+    hashType = identhashtype(srcHash)
+    calculatedHash = hashfile(srcFilePath, hashType)
+
+    if srcHash == calculatedHash:
+        print(f"\n Hashes are equal! Everything is fine. \n"
+              f"\n Reference hash type is {hashType}."
+              f"\n Input file hash is: {calculatedHash} "
+              f"\n Reference hash is: {srcHash}")
+    else:
+        print("Hashes do not correspond! \n")
+        print(f"\n Reference hash type is {hashType}."
+              f"\n Input file hash is: {calculatedHash} "
+              f"\n Reference hash is: {srcHash}")
+
+    return 0
+
+
+if __name__ == '__main__':
+    main()
